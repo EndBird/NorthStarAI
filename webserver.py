e9885eab9e71383bcf41d742178e14b6bf20f595
Minh Huynh <mlh27mlh27@yahoo.ca>
2019-11-28 12:07:32 -0500

    ..

--- webserver.py
+++ webserver.py
@@ -5,10 +5,11 @@ from flask_cors import cross_origin
 import pandas
 import pickle
 import psycopg2
+from sqlalchemy import create_engine
 
 from sklearn.preprocessing import RobustScaler
 DATABASE_URL="postgres://wfbuebdbcgdhhp:80e1efd073c910df429618085408516bf81d7468cec0eeab41af299b46b37838@ec2-23-21-248-1.compute-1.amazonaws.com:5432/d5kcgmjupq5eet"
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+engine = create_engine(DATABASE_URL)
 
 model = pickle.load(open('LogisiticRegression.sav', 'rb'))
 
@@ -33,7 +34,7 @@ def upload():
        #for some reason scaling is required for non frauds but for fraud no scaling
        preds = model.predict(data)
        data['Class'] = preds
-       data.to_sql('claimsreport', con=conn,  if_exists='append')
+       data.to_sql('claimsreport', con=engine,  if_exists='append')
 
     return "Upload Successful";
 
