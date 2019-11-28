c883639e1317fb6a332f288cf816905905d86583
Minh Huynh <mlh27mlh27@yahoo.ca>
2019-11-28 12:10:19 -0500

    ..

--- webserver.py
+++ webserver.py
@@ -35,7 +35,7 @@ def upload():
        preds = model.predict(data)
        data['Class'] = preds
        data.to_sql('claimsreport', con=engine,  if_exists='append')
-
+       engine.close()
     return "Upload Successful";
 
 
