652fe20494ff46545983ffc76839b92cd94b502a
Minh Huynh <mlh27mlh27@yahoo.ca>
2019-11-28 06:31:52 -0500

    ..

--- webserver.py
+++ webserver.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, request, jsonify, render_template
 from flask_cors import cross_origin
 import pandas
@@ -33,5 +35,6 @@ def upload():
 
 
 if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
 
-    app.run(host = '0.0.0.0')
+    app.run(host = '0.0.0.0', port=port)
